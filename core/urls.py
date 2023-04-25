809e25d4167a453c0fe7953016d1b72013808542
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-04-25 23:20:22 +0300

    create design for login and register page

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
-from .views import frontpage
+from .views import frontpage, register_user, login_user
 from django.urls import path
 
 
 app_name = 'core'
 urlpatterns = [
     path('', frontpage, name='frontpage'),
+    path('create-account/', register_user, name='register/'),
+    path('login', login_user, name='login')
 ]
--- core/views.py
+++ core/views.py
@@ -8,3 +8,10 @@ def frontpage(request):
     products = Product.objects.all()[:8]
     return render(request, 'core/index.html', {'products': products})
 
+
+def register_user(request):
+    return render(request, 'core/register.html')
+
+
+def login_user(request):
+    return render(request, 'core/login.html')
