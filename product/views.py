b425776168a57851c4e792f7f3607f5503767482
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-04-25 15:16:01 +0300

    create search logic in shopping page

--- product/views.py
+++ product/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
+from django.db.models import Q
 
 
 def shopping(request):
     products = Product.objects.all()
-    categories = Category.objects.all()
-    return render(request, 'product/shopping.html', {'products': products, 'categories': categories})
+    active_category = request.GET.get('category', '')
+    if active_category:
+        products = products.filter(category__slug=active_category)
+
+    query = request.GET.get('query', '')
+    print(query)
+    if query:
+        products = products.filter(
+            Q(name__icontains=query) | Q(content__icontains=query)
+        )
+    context = {
+        'products': products,
+        'categories': Category.objects.all(),
+        'active_category': active_category
+    }
+    return render(request, 'product/shopping.html', context=context)
 
 
 # detail
